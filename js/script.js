638983879f2c44d53e37b008b9ee7d5453e62a8e
Aleksandra <o.barecka@gmail.com>
2017-02-14 22:35:24 +0100

    done 3 sec (no mobile yet)

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-console.log('script');
+"use strict";
+
+//sticky nav
+$(document).ready(function(){
+      $(window).scroll(function() { 
+        if ($(document).scrollTop() > 50) {
+          $(".navbar-fixed-top").css("background-color", "#f9f8fb");
+        } else {
+          $(".navbar-fixed-top").css("background-color", "transparent");
+        }
+      });
+    });
